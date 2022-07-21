60214ae909e5a68c65fac9e9e884dc82085020e9
Tatsuhiko Ikemoto <qt4cpp@gmail.com>
2022-07-21 10:13:24 +0900

    Card Widget fist version.

---
+++ src/cardwidget.py
@@ -0,0 +1,50 @@
+"""Show card object. Display a text."""
+import sys
+
+from PySide6 import QtWidgets
+from PySide6.QtCore import QSize, QRect, QPoint, QPointF
+from PySide6.QtGui import QPalette, QPaintEvent, QPainter, Qt
+from PySide6.QtWidgets import QWidget, QSizePolicy, QLabel
+
+
+class CardWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # internal data
+        self._main_text: str = ''
+        # ui
+        self.main_text_label = QLabel(self)
+
+        self.setBackgroundRole(QPalette.Base)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+    def sizeHint(self) -> QSize:
+        return QSize(200, 160)
+
+    def set_main_text(self, text: str):
+        self._main_text = text
+
+    def set_format(self):
+        self.main_text_label = QLabel(self, self._main_text)
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        painter = QPainter(self)
+        font = painter.font()
+        font.setPixelSize(30)
+        painter.setFont(font)
+        contents_rect = self.contentsRect()
+        painter.drawText(contents_rect, Qt.AlignCenter,'七転び八起き')
+        pen = painter.pen()
+        pen.setStyle(Qt.DotLine)
+        painter.setPen(pen)
+        painter.drawRoundedRect(contents_rect, 20, 20)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+
+    widget = CardWidget()
+    widget.show()
+
+    sys.exit(app.exec())
