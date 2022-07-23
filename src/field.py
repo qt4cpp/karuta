0ed83a4dabec5b886f7d93ee7de49f7dd7b5f41a
Tatsuhiko Ikemoto <qt4cpp@gmail.com>
2022-07-24 07:28:43 +0900

    add Field Class

---
+++ src/field.py
@@ -0,0 +1,21 @@
+from PySide6.QtWidgets import QWidget, QGridLayout
+
+
+class Field(QWidget):
+    def __init__(self, parent=None, deck=None):
+        super().__init__(parent)
+        self._deck = deck
+
+    def set_deck(self, deck):
+        self._deck = deck
+
+    def deal(self, x, y):
+        layout = QGridLayout()
+        index = 0
+        for i in range(x):
+            for j in range(y):
+                if index >= len(self._deck):
+                    break
+                layout.addWidget(self._deck[index], i, j)
+                index += 1
+        self.setLayout(layout)
